1858450d4f9786bb15f66a5ad803fe57a1c99db4
Mathieu Méa <mathieu.mea@gmail.com>
2019-02-17 05:55:03 -0500

    BashSupport > Convert simple brackets to double brackets

--- commons.sh
+++ commons.sh
@@ -6,7 +6,7 @@ COMMONS_BEFORE_DATE_SEC=$(date +%s);
 
 # set current working directory to the directory of the script
 function setCurrentDirectory() {
-	if [ "$#" -lt 1 ]; then
+	if [[ "$#" -lt 1 ]]; then
 		echo "> setCurrentDirectory() > Illegal number of parameters!";
 		exit 1;
 	fi
@@ -16,27 +16,27 @@ function setCurrentDirectory() {
 }
 
 function checkResult() {
-	if [ "$#" -lt 1 ]; then
+	if [[ "$#" -lt 1 ]]; then
 		echo "> checkResult() > Illegal number of parameters!";
 		exit 1;
 	fi
 	local RESULT=$1;
 	local CONFIRM=false; # OFF by default
-	if [ "$#" -ge 2 ]; then
+	if [[ "$#" -ge 2 ]]; then
 		CONFIRM=$2;
 	fi
-	if [ ${RESULT} != 0 ]; then
+	if [[ ${RESULT} != 0 ]]; then
 		echo "> FAILED, AGAIN AND AGAIN, FAILED, AGAIN AND AGAIN, FAILED, AGAIN AND AGAIN";
 		exit ${RESULT};
 	else
-		if [ "$CONFIRM" == true ]; then
+		if [[ "$CONFIRM" == true ]]; then
 			read -r -n 1 -p "Continue?";
 		fi
 	fi
 }
 
 function contains() {
-	if [ "$#" -lt 2 ]; then
+	if [[ "$#" -lt 2 ]]; then
 		echo "> contains() > Illegal number of parameters!";
 		exit 1;
 	fi
@@ -45,7 +45,7 @@ function contains() {
 	local ITEM=${LIST[ITEM_IDX]};
 	unset 'LIST[ITEM_IDX]';
 	for e in "${LIST[@]}"; do
-		if [ "$e" = "$ITEM" ] ; then
+		if [[ "$e" = "$ITEM" ]] ; then
 			return 0; # CONTAINS
 		fi
 	done
@@ -53,23 +53,23 @@ function contains() {
 }
 
 function download() {
-	if [ "$#" -lt 2 ]; then
+	if [[ "$#" -lt 2 ]]; then
 		echo "> download() > Illegal number of parameters!";
 		exit 1;
 	fi
 	local URL=$1;
 	local NEW_FILE=$(basename "$URL");
 	local LAST_FILE=$2;
-	if [ -e ${LAST_FILE} ]; then
+	if [[ -e ${LAST_FILE} ]]; then
 		cp ${LAST_FILE} "${NEW_FILE}";
 		wget --header="User-Agent: MonTransit" --timeout=60 --tries=6 -N "$URL";
 	else
 		wget --header="User-Agent: MonTransit" --timeout=60 --tries=6 -S "$URL";
 	fi;
-	if [ -e "${NEW_FILE}" ]; then
-		if [ -e ${LAST_FILE} ]; then
+	if [[ -e "${NEW_FILE}" ]]; then
+		if [[ -e ${LAST_FILE} ]]; then
 			diff "${NEW_FILE}" ${LAST_FILE} >/dev/null;
-			if [ $? -eq 0 ]; then
+			if [[ $? -eq 0 ]]; then
 				rm "${NEW_FILE}"; # same file
 			else
 				mv "${NEW_FILE}" ${LAST_FILE}; # different file
