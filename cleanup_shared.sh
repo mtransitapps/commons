dab3e321fbb8f683caf499cd2aefc2ac35f095a0
mmathieum <mathieu.mea@gmail.com>
2019-10-31 06:01:01 -0400

    Shared files from /app-android/enc/

--- cleanup_shared.sh
+++ cleanup_shared.sh
@@ -12,89 +12,143 @@ CURRENT_DIRECTORY=$(basename ${CURRENT_PATH});
 
 echo "Current directory: $CURRENT_DIRECTORY";
 
+DEST_PATH=".";
 for SRC_FILE_PATH in commons/shared/* ; do
 	FILENAME=$(basename ${SRC_FILE_PATH});
+	DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 	if [[ -f $SRC_FILE_PATH ]]; then
-		DEST_PATH=".";
-		DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 		echo "--------------------------------------------------------------------------------";
 		if ! [[ -f "$DEST_FILE_PATH" ]]; then
-			echo "> File '$DEST_FILE_PATH' ($SRC_FILE_PATH) does NOT exist in target directory!";
+			echo "> File to cleanup '$DEST_FILE_PATH' ($SRC_FILE_PATH) does NOT exist in target directory!";
 			exit 1;
 		fi
 		diff -q $DEST_FILE_PATH $SRC_FILE_PATH;
 		RESULT=$?;
 		if [[ ${RESULT} -ne 0 ]]; then
-			echo "Deployed shared file $DEST_FILE_PATH changed (source: '$SRC_FILE_PATH')!";
+			echo "> Deployed shared file $DEST_FILE_PATH changed (source: '$SRC_FILE_PATH')!";
 			exit ${RESULT};
 		fi		
-		echo "> Cleaning-up '$DEST_FILE_PATH' in '$DEST_PATH'...";
+		echo "> Cleaning-up file '$DEST_FILE_PATH' in '$DEST_PATH'...";
 		rm $DEST_FILE_PATH;
 		RESULT=$?;
-		echo "> Cleaning-up '$DEST_FILE_PATH' in '$DEST_PATH'... DONE";
 		if [[ ${RESULT} -ne 0 ]]; then
-			echo "Error while cleaning-up '$DEST_FILE_PATH' to '$DEST_PATH'!";
+			echo "> Error while cleaning-up file '$DEST_FILE_PATH' to '$DEST_PATH'!";
 			exit ${RESULT};
 		fi
+		echo "> Cleaning-up file '$DEST_FILE_PATH' in '$DEST_PATH'... DONE";
 		echo "--------------------------------------------------------------------------------";
 	elif [[ -d "$SRC_FILE_PATH" ]]; then
-		DEST_PATH=".";
-		DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 		if ! [[ -d "$DEST_FILE_PATH" ]]; then
-			echo "Directory to cleanup '$DEST_FILE_PATH' not found in target directory!";
+			echo "> Directory to cleanup '$DEST_FILE_PATH' not found in target directory!";
 			exit 1;
 		fi
+		S_DEST_PATH="$DEST_PATH/${FILENAME}";
 		for S_SRC_FILE_PATH in commons/shared/${FILENAME}/* ; do
 			echo "--------------------------------------------------------------------------------";
 			S_FILENAME=$(basename ${S_SRC_FILE_PATH});
-			S_DEST_PATH="${FILENAME}";
 			S_DEST_FILE_PATH="$S_DEST_PATH/$S_FILENAME"
 			if [[ -f "$S_SRC_FILE_PATH" ]]; then
 				if ! [[ -f "$S_DEST_FILE_PATH" ]]; then
-					echo "> File '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
+					echo "> File to cleanup '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
 					exit 1;
 				fi
 				diff -q $S_DEST_FILE_PATH $S_SRC_FILE_PATH;
 				RESULT=$?;
 				if [[ ${RESULT} -ne 0 ]]; then
-					echo "Deployed shared file $S_DEST_FILE_PATH changed (source: '$S_SRC_FILE_PATH')!";
+					echo "> Deployed shared file $S_DEST_FILE_PATH changed (source: '$S_SRC_FILE_PATH')!";
 					exit ${RESULT};
 				fi		
-				echo "> Cleaning-up '$S_DEST_FILE_PATH' in '$S_DEST_PATH'...";
+				echo "> Cleaning-up file '$S_DEST_FILE_PATH' in '$S_DEST_PATH'...";
 				rm $S_DEST_FILE_PATH;
 				RESULT=$?;
-				echo "> Cleaning-up '$S_DEST_FILE_PATH' in '$S_DEST_PATH'... DONE";
 				if [[ ${RESULT} -ne 0 ]]; then
-					echo "Error while cleaning-up '$S_DEST_FILE_PATH' to '$DEST_PATH'!";
+					echo "> Error while cleaning-up file '$S_DEST_FILE_PATH' to '$S_DEST_PATH'!";
 					exit ${RESULT};
 				fi
+				echo "> Cleaning-up file '$S_DEST_FILE_PATH' in '$S_DEST_PATH'... DONE";
 				echo "--------------------------------------------------------------------------------";
 			elif [[ -d "$S_SRC_FILE_PATH" ]]; then
 				if ! [[ -d "$S_DEST_FILE_PATH" ]]; then
-					echo "> Direcotory '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
+					echo "> Direcotory to cleanup '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
 					exit 1;
 				fi
-				diff -q -r $S_DEST_FILE_PATH $S_SRC_FILE_PATH;
-				RESULT=$?;
-				if [[ ${RESULT} -ne 0 ]]; then
-					echo "Deployed shared directory $S_DEST_FILE_PATH changed (source: '$S_SRC_FILE_PATH')!";
-					exit ${RESULT};
+				SS_DEST_PATH="${S_DEST_PATH}/${S_FILENAME}";
+				for SS_SRC_FILE_PATH in commons/shared/${FILENAME}/${S_FILENAME}/* ; do
+					echo "--------------------------------------------------------------------------------";
+					SS_FILENAME=$(basename ${SS_SRC_FILE_PATH});
+					SS_DEST_FILE_PATH="$SS_DEST_PATH/$SS_FILENAME"
+					if [[ -f "$SS_SRC_FILE_PATH" ]]; then
+						if ! [[ -f "$SS_DEST_FILE_PATH" ]]; then
+							echo "> File to cleanup '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) does NOT exist in target directory!";
+							exit 1;
+						fi
+						diff -q $SS_DEST_FILE_PATH $SS_SRC_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Deployed shared file $SS_DEST_FILE_PATH changed (source: '$SS_SRC_FILE_PATH')!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up file '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'...";
+						rm $SS_DEST_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Error while cleaning-up file '$SS_DEST_FILE_PATH' to '$SS_DEST_PATH'!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up file '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'... DONE";
+						echo "--------------------------------------------------------------------------------";
+					elif [[ -d "$S_SRC_FILE_PATH" ]]; then
+						if ! [[ -d "$SS_DEST_FILE_PATH" ]]; then
+							echo "> Direcotory to cleanup '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) does NOT exist in target directory!";
+							exit 1;
+						fi
+						diff -q -r $SS_DEST_FILE_PATH $SS_SRC_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Deployed shared directory $SS_DEST_FILE_PATH changed (source: '$SS_SRC_FILE_PATH')!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up directory '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'...";
+						rm -r $SS_DEST_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Error while cleaning-up directory '$SS_DEST_FILE_PATH' to '$SS_DEST_PATH'!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up directory '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'... DONE";
+						echo "--------------------------------------------------------------------------------";
+					else #WTF
+						echo "> File to cleanup '$SS_FILENAME' ($SS_SRC_FILE_PATH) is neither a directory or a file!";
+						ls -l $SS_FILENAME;
+						exit 1;
+					fi
+				done
+				if ! [[ "$(ls -A $S_DEST_FILE_PATH)" ]]; then
+					echo "> Deleting empty directory '$S_DEST_FILE_PATH'...";
+					rm -r $S_DEST_FILE_PATH;
+					RESULT=$?;
+					if [[ ${RESULT} -ne 0 ]]; then
+						echo "> Error while deleting empty directory '$S_DEST_FILE_PATH'!";
+						exit ${RESULT};
+					fi
+					echo "> Deleting empty directory '$S_DEST_FILE_PATH'... DONE";
 				fi
-				echo "> Cleaning-up '$S_DEST_FILE_PATH' in '$S_DEST_PATH'...";
-				rm -r $S_DEST_FILE_PATH;
-				RESULT=$?;
-				echo "> Cleaning-up '$S_DEST_FILE_PATH' in '$S_DEST_PATH'... DONE";
-				if [[ ${RESULT} -ne 0 ]]; then
-					echo "Error while cleaning-up '$S_DEST_FILE_PATH' to '$DEST_PATH'!";
-					exit ${RESULT};
-				fi
-				echo "--------------------------------------------------------------------------------";
 			else #WTF
 				echo "> File to cleanup '$S_FILENAME' ($S_SRC_FILE_PATH) is neither a directory or a file!";
 				ls -l $S_FILENAME;
 				exit 1;
 			fi
 		done
+		if ! [[ "$(ls -A $DEST_FILE_PATH)" ]]; then
+			echo "> Deleting empty directory '$DEST_FILE_PATH'...";
+			rm -r $DEST_FILE_PATH;
+			RESULT=$?;
+			if [[ ${RESULT} -ne 0 ]]; then
+				echo "> Error while deleting empty directory '$DEST_FILE_PATH'!";
+				exit ${RESULT};
+			fi
+			echo "> Deleting empty directory '$DEST_FILE_PATH'... DONE";
+		fi
 	else #WTF
 		echo "> File to cleanup '$FILENAME' ($SRC_FILE_PATH) is neither a directory or a file!";
 		ls -l $FILENAME;
--- deploy_shared.sh
+++ deploy_shared.sh
@@ -12,11 +12,11 @@ CURRENT_DIRECTORY=$(basename ${CURRENT_PATH});
 
 echo "Current directory: $CURRENT_DIRECTORY";
 
+DEST_PATH=".";
 for SRC_FILE_PATH in commons/shared/* ; do
 	FILENAME=$(basename ${SRC_FILE_PATH});
+	DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 	if [[ -f $SRC_FILE_PATH ]]; then
-		DEST_PATH=".";
-		DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 		echo "--------------------------------------------------------------------------------";
 		if [[ -f "$DEST_FILE_PATH" ]]; then
 			echo "> File '$DEST_FILE_PATH' ($SRC_FILE_PATH) exists in target directory!";
@@ -28,25 +28,22 @@ for SRC_FILE_PATH in commons/shared/* ; do
 		RESULT=$?;
 		echo "> Deploying '$SRC_FILE_PATH' in '$DEST_PATH'... DONE";
 		if [[ ${RESULT} -ne 0 ]]; then
-			echo "Error while deploying '$SRC_FILE_PATH' to '$DEST_PATH'!";
+			echo "> Error while deploying '$SRC_FILE_PATH' to '$DEST_PATH'!";
 			exit ${RESULT};
 		fi
 		echo "--------------------------------------------------------------------------------";
 	elif [[ -d "$SRC_FILE_PATH" ]]; then
-		DEST_PATH=".";
-		DEST_FILE_PATH="$DEST_PATH/$FILENAME"
 		if ! [[ -d "$DEST_FILE_PATH" ]]; then
 			mkdir $DEST_FILE_PATH;
 			RESULT=$?;
 			if [[ ${RESULT} -ne 0 ]]; then
-				echo "Error while creating directory '$DEST_FILE_PATH' in target directory!";
+				echo "> Error while creating directory '$DEST_FILE_PATH' in target directory!";
 				exit ${RESULT};
 			fi
 		fi
+		S_DEST_PATH="${DEST_PATH}/${FILENAME}";
 		for S_SRC_FILE_PATH in commons/shared/${FILENAME}/* ; do
-			echo "--------------------------------------------------------------------------------";
 			S_FILENAME=$(basename ${S_SRC_FILE_PATH});
-			S_DEST_PATH="${FILENAME}";
 			S_DEST_FILE_PATH="$S_DEST_PATH/$S_FILENAME"
 			if [[ -f "$S_DEST_FILE_PATH" ]]; then
 				echo "> File '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) exists in target directory!";
@@ -54,19 +51,43 @@ for SRC_FILE_PATH in commons/shared/* ; do
 				exit 1;
 			fi
 			if [[ -d "$S_DEST_FILE_PATH" ]]; then
-				echo "> Directory '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) exists in target directory!";
-				ls -l $S_DEST_FILE_PATH;
-				exit 1;
+				SS_DEST_PATH="${S_DEST_PATH}/${S_FILENAME}";
+				for SS_SRC_FILE_PATH in commons/shared/${FILENAME}/${S_FILENAME}/* ; do
+					SS_FILENAME=$(basename ${SS_SRC_FILE_PATH});
+					SS_DEST_FILE_PATH="$SS_DEST_PATH/$SS_FILENAME"
+					if [[ -f "$SS_DEST_FILE_PATH" ]]; then
+						echo "> File '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) exists in target directory!";
+						ls -l $SS_DEST_FILE_PATH;
+						exit 1;
+					fi
+					if [[ -d "$SS_DEST_FILE_PATH" ]]; then
+						echo "> Directory '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) exists in target directory!";
+						ls -l $SS_DEST_FILE_PATH;
+						exit 1;
+					fi
+					echo "--------------------------------------------------------------------------------";
+					echo "> Deploying '$SS_SRC_FILE_PATH' in '$SS_DEST_PATH'...";
+					cp -nR $SS_SRC_FILE_PATH $SS_DEST_PATH/;
+					RESULT=$?;
+					echo "> Deploying '$SS_SRC_FILE_PATH' in '$SS_DEST_PATH'... DONE";
+					if [[ ${RESULT} -ne 0 ]]; then
+						echo "Error while deploying '$SS_SRC_FILE_PATH' to '$SS_FILENAME'!";
+						exit ${RESULT};
+					fi
+					echo "--------------------------------------------------------------------------------";
+				done
+			else
+				echo "--------------------------------------------------------------------------------";
+				echo "> Deploying '$S_SRC_FILE_PATH' in '$S_DEST_PATH'...";
+				cp -nR $S_SRC_FILE_PATH $S_DEST_PATH/;
+				RESULT=$?;
+				echo "> Deploying '$S_SRC_FILE_PATH' in '$S_DEST_PATH'... DONE";
+				if [[ ${RESULT} -ne 0 ]]; then
+					echo "> Error while deploying '$S_SRC_FILE_PATH' to '$S_FILENAME'!";
+					exit ${RESULT};
+				fi
+				echo "--------------------------------------------------------------------------------";
 			fi
-			echo "> Deploying '$S_SRC_FILE_PATH' in '$S_DEST_PATH'...";
-			cp -nR $S_SRC_FILE_PATH $S_DEST_PATH/;
-			RESULT=$?;
-			echo "> Deploying '$S_SRC_FILE_PATH' in '$S_DEST_PATH'... DONE";
-			if [[ ${RESULT} -ne 0 ]]; then
-				echo "Error while deploying '$S_SRC_FILE_PATH' to '$S_FILENAME'!";
-				exit ${RESULT};
-			fi
-			echo "--------------------------------------------------------------------------------";
 		done
 	else #WTF
 		echo "> File to deploy '$FILENAME' ($SRC_FILE_PATH) is neither a directory or a file!";
