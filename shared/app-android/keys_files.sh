03f720f4a76cffd3363151c94db457484a9c2d42
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-02 08:51:54 -0400

    Move app-android/keys_files.sh to commons/shared/

---
+++ shared/app-android/keys_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+declare -a FILES=(
+	"app-signing-release-keystore.keystore"
+	"app-signing-release-keys.properties"
+	"app-signing-release-keystore-encrypted.keystore"
+	"google-play-upload-certificate.pem"
+	"google-play-upload-keystore.keystore"
+	"google-play-upload-keys.properties"
+);
+
+if [[ -f "res/values/keys.xml" ]]; then
+	FILES+=("res/values/keys.xml");
+fi
+
+if [[ -f "google-services.json" ]]; then
+	FILES+=("google-services.json");
+fi
+
+echo "Files:";
+printf '* "%s"\n' "${FILES[@]}";
