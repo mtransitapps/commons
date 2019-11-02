c3e6607544d75b84cd2d8ba87261d6b35c76ef50
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-01 20:17:04 -0400

    Cleanup optional shared files

--- cleanup.sh
+++ cleanup.sh
@@ -158,6 +158,150 @@ for SRC_FILE_PATH in ${SRC_DIR_PATH}/* ; do
 	fi
 done
 
+SRC_DIR_PATH="commons/shared-opt-dir";
+for SRC_FILE_PATH in ${SRC_DIR_PATH}/* ; do
+	FILENAME=$(basename ${SRC_FILE_PATH});
+	DEST_FILE_PATH="$DEST_PATH/$FILENAME"
+	if [[ -f $SRC_FILE_PATH ]]; then
+		echo "--------------------------------------------------------------------------------";
+		if ! [[ -f "$DEST_FILE_PATH" ]]; then
+			echo "> File to cleanup '$DEST_FILE_PATH' ($SRC_FILE_PATH) does NOT exist in target directory!";
+			exit 1;
+		fi
+		diff -q $DEST_FILE_PATH $SRC_FILE_PATH;
+		RESULT=$?;
+		if [[ ${RESULT} -ne 0 ]]; then
+			echo "> Deployed shared file $DEST_FILE_PATH changed (source: '$SRC_FILE_PATH')!";
+			exit ${RESULT};
+		fi
+		echo "> Cleaning-up file '$DEST_FILE_PATH' in '$DEST_PATH'...";
+		rm $DEST_FILE_PATH;
+		RESULT=$?;
+		if [[ ${RESULT} -ne 0 ]]; then
+			echo "> Error while cleaning-up file '$DEST_FILE_PATH' to '$DEST_PATH'!";
+			exit ${RESULT};
+		fi
+		echo "> Cleaning-up file '$DEST_FILE_PATH' in '$DEST_PATH'... DONE";
+		echo "--------------------------------------------------------------------------------";
+	elif [[ -d "$SRC_FILE_PATH" ]]; then
+		if ! [[ -d "$DEST_FILE_PATH" ]]; then
+			echo "> Directory to cleanup '$DEST_FILE_PATH' not found in target directory!";
+			exit 1;
+		fi
+		S_DEST_PATH="$DEST_PATH/${FILENAME}";
+		for S_SRC_FILE_PATH in ${SRC_DIR_PATH}/${FILENAME}/* ; do
+			echo "--------------------------------------------------------------------------------";
+			S_FILENAME=$(basename ${S_SRC_FILE_PATH});
+			S_DEST_FILE_PATH="$S_DEST_PATH/$S_FILENAME"
+			if [[ -f "$S_SRC_FILE_PATH" ]]; then
+				if ! [[ -f "$S_DEST_FILE_PATH" ]]; then
+					echo "> File to cleanup '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
+					exit 1;
+				fi
+				diff -q $S_DEST_FILE_PATH $S_SRC_FILE_PATH;
+				RESULT=$?;
+				if [[ ${RESULT} -ne 0 ]]; then
+					echo "> Deployed shared file $S_DEST_FILE_PATH changed (source: '$S_SRC_FILE_PATH')!";
+					exit ${RESULT};
+				fi
+				echo "> Cleaning-up file '$S_DEST_FILE_PATH' in '$S_DEST_PATH'...";
+				rm $S_DEST_FILE_PATH;
+				RESULT=$?;
+				if [[ ${RESULT} -ne 0 ]]; then
+					echo "> Error while cleaning-up file '$S_DEST_FILE_PATH' to '$S_DEST_PATH'!";
+					exit ${RESULT};
+				fi
+				echo "> Cleaning-up file '$S_DEST_FILE_PATH' in '$S_DEST_PATH'... DONE";
+				echo "--------------------------------------------------------------------------------";
+			elif [[ -d "$S_SRC_FILE_PATH" ]]; then
+				if ! [[ -d "$S_DEST_FILE_PATH" ]]; then
+					echo "> Direcotory to cleanup '$S_DEST_FILE_PATH' ($S_SRC_FILE_PATH) does NOT exist in target directory!";
+					exit 1;
+				fi
+				SS_DEST_PATH="${S_DEST_PATH}/${S_FILENAME}";
+				for SS_SRC_FILE_PATH in ${SRC_DIR_PATH}/${FILENAME}/${S_FILENAME}/* ; do
+					echo "--------------------------------------------------------------------------------";
+					SS_FILENAME=$(basename ${SS_SRC_FILE_PATH});
+					SS_DEST_FILE_PATH="$SS_DEST_PATH/$SS_FILENAME"
+					if [[ -f "$SS_SRC_FILE_PATH" ]]; then
+						if ! [[ -f "$SS_DEST_FILE_PATH" ]]; then
+							echo "> File to cleanup '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) does NOT exist in target directory!";
+							exit 1;
+						fi
+						diff -q $SS_DEST_FILE_PATH $SS_SRC_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Deployed shared file $SS_DEST_FILE_PATH changed (source: '$SS_SRC_FILE_PATH')!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up file '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'...";
+						rm $SS_DEST_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Error while cleaning-up file '$SS_DEST_FILE_PATH' to '$SS_DEST_PATH'!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up file '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'... DONE";
+						echo "--------------------------------------------------------------------------------";
+					elif [[ -d "$S_SRC_FILE_PATH" ]]; then
+						if ! [[ -d "$SS_DEST_FILE_PATH" ]]; then
+							echo "> Direcotory to cleanup '$SS_DEST_FILE_PATH' ($SS_SRC_FILE_PATH) does NOT exist in target directory!";
+							exit 1;
+						fi
+						diff -q -r $SS_DEST_FILE_PATH $SS_SRC_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Deployed shared directory $SS_DEST_FILE_PATH changed (source: '$SS_SRC_FILE_PATH')!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up directory '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'...";
+						rm -r $SS_DEST_FILE_PATH;
+						RESULT=$?;
+						if [[ ${RESULT} -ne 0 ]]; then
+							echo "> Error while cleaning-up directory '$SS_DEST_FILE_PATH' to '$SS_DEST_PATH'!";
+							exit ${RESULT};
+						fi
+						echo "> Cleaning-up directory '$SS_DEST_FILE_PATH' in '$SS_DEST_PATH'... DONE";
+						echo "--------------------------------------------------------------------------------";
+					else #WTF
+						echo "> File to cleanup '$SS_FILENAME' ($SS_SRC_FILE_PATH) is neither a directory or a file!";
+						ls -l $SS_FILENAME;
+						exit 1;
+					fi
+				done
+				if ! [[ "$(ls -A $S_DEST_FILE_PATH)" ]]; then
+					echo "> Deleting empty directory '$S_DEST_FILE_PATH'...";
+					rm -r $S_DEST_FILE_PATH;
+					RESULT=$?;
+					if [[ ${RESULT} -ne 0 ]]; then
+						echo "> Error while deleting empty directory '$S_DEST_FILE_PATH'!";
+						exit ${RESULT};
+					fi
+					echo "> Deleting empty directory '$S_DEST_FILE_PATH'... DONE";
+				fi
+			else #WTF
+				echo "> File to cleanup '$S_FILENAME' ($S_SRC_FILE_PATH) is neither a directory or a file!";
+				ls -l $S_FILENAME;
+				exit 1;
+			fi
+		done
+		if ! [[ "$(ls -A $DEST_FILE_PATH)" ]]; then
+			echo "> Deleting empty directory '$DEST_FILE_PATH'...";
+			rm -r $DEST_FILE_PATH;
+			RESULT=$?;
+			if [[ ${RESULT} -ne 0 ]]; then
+				echo "> Error while deleting empty directory '$DEST_FILE_PATH'!";
+				exit ${RESULT};
+			fi
+			echo "> Deleting empty directory '$DEST_FILE_PATH'... DONE";
+		fi
+	else #WTF
+		echo "> File to cleanup '$FILENAME' ($SRC_FILE_PATH) is neither a directory or a file!";
+		ls -l $FILENAME;
+		exit 1;
+	fi
+done
+
 echo "--------------------------------------------------------------------------------";
 AFTER_DATE=$(date +%D-%X);
 AFTER_DATE_SEC=$(date +%s);
