080b772ab9057f5678b6ef191f0dee4011e9aa6e
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-03 10:52:59 -0500

    Cleanup parse java -> gradlew run command.

--- shared-opt-dir/agency-parser/parse_current.sh
+++ shared-opt-dir/agency-parser/parse_current.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 echo ">> Parsing Current...";
+
+IS_CI=false;
+if [[ ! -z "${CI}" ]]; then
+	IS_CI=true;
+fi
+echo "/build.sh > IS_CI:'${IS_CI}'";
+
+GRADLE_ARGS="";
+if [[ ${IS_CI} = true ]]; then
+	GRADLE_ARGS=" --console=plain";
+fi
+
 GTFS_ZIP="input/gtfs.zip";
 CHANGE_DIRECTORY=$(cat "change_directory");
 RES_DIR="res-current"
 ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw current_"
+
 ../gradlew run \
---args="${ARGS}";
+--args="${ARGS}" \
+${GRADLE_ARGS};
 RESULT=$?;
+
 echo ">> Parsing Current... DONE";
 exit ${RESULT};
--- shared-opt-dir/agency-parser/parse_next.sh
+++ shared-opt-dir/agency-parser/parse_next.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 echo ">> Parsing Next...";
+
+IS_CI=false;
+if [[ ! -z "${CI}" ]]; then
+	IS_CI=true;
+fi
+echo "/build.sh > IS_CI:'${IS_CI}'";
+
+GRADLE_ARGS="";
+if [[ ${IS_CI} = true ]]; then
+	GRADLE_ARGS=" --console=plain";
+fi
+
 GTFS_ZIP="input/gtfs_next.zip";
 if ! [[ -e "input_url_next" ]]; then
 	GTFS_ZIP="input/gtfs.zip";
@@ -7,8 +19,11 @@ fi
 CHANGE_DIRECTORY=$(cat "change_directory");
 RES_DIR="res-next"
 ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw next_"
+
 ../gradlew run \
---args="${ARGS}";
+--args="${ARGS}" \
+${GRADLE_ARGS};
 RESULT=$?;
+
 echo ">> Parsing Next... DONE";
 exit ${RESULT};
