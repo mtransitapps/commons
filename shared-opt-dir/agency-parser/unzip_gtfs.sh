cd81ec02c3ce2a5688ffb34ad811de34b2a1d43d
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-12 21:53:13 -0500

    Cleanup before solving for stop_times memory issues

--- shared-opt-dir/agency-parser/unzip_gtfs.sh
+++ shared-opt-dir/agency-parser/unzip_gtfs.sh
@@ -2,10 +2,10 @@
 source ../commons/commons.sh
 echo ">> Unzipping GTFS files...";
 TARGET_DIR="input/gtfs";
-if [[ -d $TARGET_DIR ]]; then
-	rm -r $TARGET_DIR;
+if [[ -d ${TARGET_DIR} ]]; then
+	rm -r ${TARGET_DIR};
 	checkResult $? false;
 fi
-unzip input/gtfs.zip -d $TARGET_DIR;
+unzip input/gtfs.zip -d ${TARGET_DIR};
 checkResult $? false;
 echo ">> Unzipping GTFS files...... DONE";
