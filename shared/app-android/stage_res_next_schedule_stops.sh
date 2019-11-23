2e60260e1c01abdf12eaff2c99bd1a36fd70eae7
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-23 13:36:00 -0500

    November 23 update

--- shared/app-android/stage_res_next_schedule_stops.sh
+++ shared/app-android/stage_res_next_schedule_stops.sh
@@ -3,10 +3,10 @@ source ../commons/commons.sh
 
 git add src/main/res-next/raw/next_gtfs_schedule_stop_*;
 
-git ls-files --deleted src/main/res-next/raw/next_gtfs_schedule_stop_* &> /dev/null;
+git ls-files --deleted --error-unmatch src/main/res-next/raw/ &> /dev/null;
 RESULT=$?;
 
-if [[ ${RESULT} -ne 0 ]]; then
-	git rm $(git ls-files --deleted src/main/res-next/raw/next_gtfs_schedule_stop_*);
+if [[ ${RESULT} -eq 0 ]]; then
+	git rm $(git ls-files --deleted src/main/res-next/raw/);
 	checkResult $?;
 fi
