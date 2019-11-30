e8580ca1b33ef31e9f7f63b2b2b53a56042ed8e7
Mathieu Méa <mathieu.mea@gmail.com>
2019-11-29 19:40:39 -0500

    Generate stop times from frequencies options

--- shared-opt-dir/agency-parser/parse_current.sh
+++ shared-opt-dir/agency-parser/parse_current.sh
@@ -4,10 +4,16 @@ echo ">> Parsing Current...";
 
 setGradleArgs;
 
+GENERATE_STOP_TIMES_FROM_FREQUENCIES="";
+if [[ -f "generate_stop_times_from_frequencies" ]]; then
+	GENERATE_STOP_TIMES_FROM_FREQUENCIES=$(cat "generate_stop_times_from_frequencies");
+fi
+
 GTFS_ZIP="input/gtfs.zip";
 CHANGE_DIRECTORY=$(cat "change_directory");
-RES_DIR="res-current"
-ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw current_"
+RES_DIR="res-current";
+
+ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw current_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES";
 
 ../gradlew run \
 --args="${ARGS}" \
--- shared-opt-dir/agency-parser/parse_next.sh
+++ shared-opt-dir/agency-parser/parse_next.sh
@@ -4,13 +4,19 @@ echo ">> Parsing Next...";
 
 setGradleArgs;
 
+GENERATE_STOP_TIMES_FROM_FREQUENCIES="";
+if [[ -f "generate_stop_times_from_frequencies" ]]; then
+	GENERATE_STOP_TIMES_FROM_FREQUENCIES=$(cat "generate_stop_times_from_frequencies");
+fi
+
 GTFS_ZIP="input/gtfs_next.zip";
 if ! [[ -e "input_url_next" ]]; then
 	GTFS_ZIP="input/gtfs.zip";
 fi
 CHANGE_DIRECTORY=$(cat "change_directory");
-RES_DIR="res-next"
-ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw next_"
+RES_DIR="res-next";
+
+ARGS="$GTFS_ZIP $CHANGE_DIRECTORY$RES_DIR/raw next_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES";
 
 ../gradlew run \
 --args="${ARGS}" \
