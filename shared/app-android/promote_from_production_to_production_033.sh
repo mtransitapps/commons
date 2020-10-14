e57910f2647b32e362548fc1e5eeb3d8421580a4
Mathieu Méa <mathieu.mea@gmail.com>
2020-10-14 19:55:10 -0400

    Update Android Gradle plugin to 4.1.0, Gradle Wrapper to 6.5.1 & Play Publisher to 3.0.0

--- shared/app-android/promote_from_production_to_production_033.sh
+++ shared/app-android/promote_from_production_to_production_033.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./promote.sh --from-track production --promote-track production --update production --track production --user-fraction 0.33 --release-status inProgress;
+./promote.sh --from-track production --promote-track production --update production --user-fraction 0.33 --release-status inProgress;
