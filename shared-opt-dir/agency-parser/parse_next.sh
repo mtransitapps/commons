cbca7d81c42a302e6bc1617a995c26dc2f2e2fbb
Mathieu Méa <mathieu.mea@gmail.com>
2021-10-08 09:44:11 -0400

    Ignore invalid GTFS next file.

--- shared-opt-dir/agency-parser/parse_next.sh
+++ shared-opt-dir/agency-parser/parse_next.sh
@@ -13,6 +13,9 @@ GTFS_ZIP="input/gtfs_next.zip";
 if ! [[ -e "input_url_next" ]]; then
 	GTFS_ZIP="input/gtfs.zip";
 fi
+if ! [[ -e "input/gtfs_next/agency.txt" ]]; then
+	GTFS_ZIP="input/gtfs.zip";
+fi
 
 ARGS="$GTFS_ZIP unused next_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES";
 
