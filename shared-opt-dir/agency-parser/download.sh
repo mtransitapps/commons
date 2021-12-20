2cdc5bd3affffa5794cfce17a2ff843623046caf
Mathieu Méa <mathieu.mea@gmail.com>
2021-12-20 09:58:20 -0500

    Parser: compatibility with "config" folder

--- shared-opt-dir/agency-parser/download.sh
+++ shared-opt-dir/agency-parser/download.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 source ../commons/commons.sh
 echo ">> Downloading..."
-URL=`cat input_url`;
+
+FILE_PATH=".";
+if [[ -d "../app-android/config" ]]; then
+	FILE_PATH="../app-android/config";
+fi
+
+URL=`cat $FILE_PATH/input_url`;
 mkdir -p input;
 download "${URL}" "input/gtfs.zip";
 checkResult $?;
-if [[ -e "input_url_next" ]]; then
-	URL=`cat input_url_next`;
+if [[ -e "$FILE_PATH/input_url_next" ]]; then
+	URL=`cat $FILE_PATH/input_url_next`;
 	download "${URL}" "input/gtfs_next.zip";
 	checkResult $?;
 fi
