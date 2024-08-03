c12b23494f8fd71992e7d807ce126be0f0b11572
Mathieu Méa <mathieu.mea@gmail.com>
2024-08-02 21:16:41 -0400

    GTFS Archive: compat with next URL (future is inside current??)

--- shared-opt-dir/agency-parser/archive.sh
+++ shared-opt-dir/agency-parser/archive.sh
@@ -23,6 +23,9 @@ if [[ ! -d "${FILES_DIR}" ]]; then
   exit 1;
 fi
 
+YESTERDAY=$(date -d "yesterday" +%Y%m%d); # service can start yesterday and finish today
+echo "> Yesterday: '$YESTERDAY'";
+
 START_DATE=""
 END_DATE=""
 FILE_CALENDAR="${FILES_DIR}/calendar.txt";
@@ -45,9 +48,9 @@ if [[ -f "$FILE_CALENDAR" ]]; then
   readarray -t START_DATES_SORTED < <(printf '%s\n' "${START_DATES[@]}" | sort)
   readarray -t END_DATES_SORTED < <(printf '%s\n' "${END_DATES[@]}" | sort)
   START_DATE=${START_DATES_SORTED[0]}
-  echo "- start date: ${START_DATE}"
   END_DATE=${END_DATES_SORTED[-1]}
-  echo "- end date: ${END_DATE}"
+  echo "- start date: '${START_DATE}'"
+  echo "- end date: '${END_DATE}'"
 elif [[ -f "$FILE_CALENDAR_DATES" ]]; then
   echo "- Using $FILE_CALENDAR_DATES...";
   HEADERS=$(head -n 1 "$FILE_CALENDAR_DATES" | tr -d '\r')
@@ -60,8 +63,8 @@ elif [[ -f "$FILE_CALENDAR_DATES" ]]; then
   cleanArray DATES
   readarray -t DATES_SORTED < <(printf '%s\n' "${DATES[@]}" | sort)
   START_DATE=${DATES_SORTED[0]}
-  echo "- start date: '${START_DATE}'"
   END_DATE=${DATES_SORTED[-1]}
+  echo "- start date: '${START_DATE}'"
   echo "- end date: '${END_DATE}'"
 else
   echo "ERROR: GTFS files not found in ${FILES_DIR}";
@@ -74,12 +77,10 @@ if [[ -z "$START_DATE" || -z "$END_DATE" ]]; then
 fi
 
 ARCHIVE_DIR="${SCRIPT_DIR}/archive";
-echo "- archive dir: $ARCHIVE_DIR";
+echo "- archive dir: '$ARCHIVE_DIR'";
 
 mkdir -p "$ARCHIVE_DIR";
 
-YESTERDAY=$(date -d "yesterday" +%Y%m%d); # service can start yesterday and finish today
-echo "- yesterday: $YESTERDAY";
 
 ZIP_FILE_COUNT=$(find $ARCHIVE_DIR -name "*.zip" -type f | wc -l);
 if [[ "$ZIP_FILE_COUNT" -gt 0 ]]; then
@@ -91,24 +92,30 @@ if [[ "$ZIP_FILE_COUNT" -gt 0 ]]; then
     ZIP_FILE_BASENAME_NO_EXT_PARTS=(${ZIP_FILE_BASENAME_NO_EXT//-/ });
     ZIP_FILE_START_DATE=${ZIP_FILE_BASENAME_NO_EXT_PARTS[0]};
     ZIP_FILE_END_DATE=${ZIP_FILE_BASENAME_NO_EXT_PARTS[1]};
-    echo "- ZIP start date: $ZIP_FILE_START_DATE";
-    if [[ "$ZIP_FILE_START_DATE" -lt "$YESTERDAY" && "$ZIP_FILE_END_DATE" -ge "$YESTERDAY" ]]; then
-      ZIP_FILE_START_DATE=$YESTERDAY;
-      echo "- ZIP start date (yesterday): $ZIP_FILE_START_DATE";
-    fi
-    echo "- ZIP end date: $ZIP_FILE_END_DATE";
-    if [[ "$ZIP_FILE_END_DATE" -lt "$YESTERDAY" && "$ZIP_FILE_END_DATE" -le "$START_DATE" ]]; then
+    echo "- ZIP start date: '$ZIP_FILE_START_DATE'";
+    echo "- ZIP end date: '$ZIP_FILE_END_DATE'";
+    if [[ "$ZIP_FILE_END_DATE" -lt "$YESTERDAY" && "$ZIP_FILE_END_DATE" -lt "$START_DATE" ]]; then
       echo "- ZIP file is entirely in the past and older than new ZIP > REMOVE";
+      rm "$ZIP_FILE";
+      checkResult $?;
+    elif [[ "$ZIP_FILE_START_DATE" -eq "$START_DATE" && "$ZIP_FILE_END_DATE" -eq "$END_DATE" ]]; then
+      echo "- ZIP file is the same as the new ZIP > REMOVE";
       rm "$ZIP_FILE";
       checkResult $?;
     elif [[ "$ZIP_FILE_START_DATE" -ge "$START_DATE" && "$ZIP_FILE_END_DATE" -le "$END_DATE" ]]; then
       echo "- ZIP file is entirely inside the new ZIP > REMOVE";
       rm "$ZIP_FILE";
       checkResult $?;
+    elif [[ "$ZIP_FILE_START_DATE" -lt "$YESTERDAY" && "$ZIP_FILE_END_DATE" -le "$END_DATE" ]]; then
+      echo "- ZIP file (after $YESTERDAY) is entirely inside the new ZIP > REMOVE";
+      rm "$ZIP_FILE";
+      checkResult $?;
     elif [[ "$ZIP_FILE_START_DATE" -gt "$END_DATE" && "$ZIP_FILE_END_DATE" -gt "$YESTERDAY" ]]; then
       echo "- ZIP file is entirely in the future and newer than new ZIP > KEEP";
     else
-      echo "TODO handle this case";
+      echo "- TODO handle this case?";
+      # - new ZIP file (future) is entirely inside archive ZIP file (current)
+      # TODO ? exit 1;
     fi
     echo "--------------------"
   done
