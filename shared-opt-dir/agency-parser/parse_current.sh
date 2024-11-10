468e565e23c5b5ff6c64639b7425fb1516245163
Mathieu Méa <mathieu.mea@gmail.com>
2024-11-10 18:05:51 -0500

    Send input URL to parsing script + generate from frequencies default value "false"

--- shared-opt-dir/agency-parser/parse_current.sh
+++ shared-opt-dir/agency-parser/parse_current.sh
@@ -5,14 +5,23 @@ echo ">> Parsing Current...";
 
 setGradleArgs;
 
-GENERATE_STOP_TIMES_FROM_FREQUENCIES="";
+FILE_PATH="${SCRIPT_DIR}";
+if [[ -d "${SCRIPT_DIR}/../config" ]]; then
+	FILE_PATH="${SCRIPT_DIR}/../config";
+elif [[ -d "${SCRIPT_DIR}/../app-android/config" ]]; then # OLD REPO
+	FILE_PATH="${SCRIPT_DIR}/../app-android/config";
+fi
+
+URL=$(cat "$FILE_PATH/input_url");
+
+GENERATE_STOP_TIMES_FROM_FREQUENCIES="false";
 if [[ -f "${SCRIPT_DIR}/generate_stop_times_from_frequencies" ]]; then
 	GENERATE_STOP_TIMES_FROM_FREQUENCIES=$(cat "${SCRIPT_DIR}/generate_stop_times_from_frequencies");
 fi
 
 GTFS_DIR="input/gtfs";
 
-ARGS="$GTFS_DIR unused current_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES";
+ARGS="$GTFS_DIR unused current_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES $URL";
 
 ${SCRIPT_DIR}/../gradlew run \
 --args="${ARGS}" \
--- shared-opt-dir/agency-parser/parse_next.sh
+++ shared-opt-dir/agency-parser/parse_next.sh
@@ -5,7 +5,19 @@ echo ">> Parsing Next...";
 
 setGradleArgs;
 
-GENERATE_STOP_TIMES_FROM_FREQUENCIES="";
+FILE_PATH="${SCRIPT_DIR}";
+if [[ -d "${SCRIPT_DIR}/../config" ]]; then
+	FILE_PATH="${SCRIPT_DIR}/../config";
+elif [[ -d "${SCRIPT_DIR}/../app-android/config" ]]; then # OLD REPO
+	FILE_PATH="${SCRIPT_DIR}/../app-android/config";
+fi
+
+URL=$(cat "$FILE_PATH/input_url");
+if [[ -e "$FILE_PATH/input_url_next" ]]; then
+ URL=$(cat "$FILE_PATH/input_url_next");
+fi
+
+GENERATE_STOP_TIMES_FROM_FREQUENCIES="false";
 if [[ -f "${SCRIPT_DIR}/generate_stop_times_from_frequencies" ]]; then
 	GENERATE_STOP_TIMES_FROM_FREQUENCIES=$(cat "${SCRIPT_DIR}/generate_stop_times_from_frequencies");
 fi
@@ -23,7 +35,7 @@ if ! [[ -e "${SCRIPT_DIR}/input/gtfs_next/agency.txt" ]]; then
 	GTFS_DIR="input/gtfs";
 fi
 
-ARGS="$GTFS_DIR unused next_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES";
+ARGS="$GTFS_DIR unused next_ $GENERATE_STOP_TIMES_FROM_FREQUENCIES $URL";
 
 ${SCRIPT_DIR}/../gradlew run \
 --args="${ARGS}" \
