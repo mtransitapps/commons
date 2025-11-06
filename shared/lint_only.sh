78aae33200b9f03198599b334901966ac5d89e6f
Mathieu Méa <mathieu.mea@gmail.com>
2025-11-05 21:18:10 -0500

    Split `test.sh` in 2: `test_only.sh` & `lint_only.sh` (#464)

--- shared/lint_only.sh
+++ shared/lint_only.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 source commons/commons.sh;
 echo "================================================================================";
-echo "> TEST ALL...";
+echo "> LINT...";
 echo "--------------------------------------------------------------------------------";
 BEFORE_DATE=$(date +%D-%X);
 BEFORE_DATE_SEC=$(date +%s);
 
-./test_only.sh
-checkResult $?
+setIsCI;
 
-./lint_only.sh
-checkResult $?
+setGradleArgs;
+
+setGitProjectName;
+
+cd app-android || exit;
+
+if [[ $PROJECT_NAME == "mtransit-for-android" ]]; then
+
+	echo ">> Running lint...";
+	../gradlew :app-android:lintDebug ${GRADLE_ARGS};
+	checkResult $?;
+	echo ">> Running lint... DONE";
+fi
+
+cd ..;
 
 AFTER_DATE=$(date +%D-%X);
 AFTER_DATE_SEC=$(date +%s);
 DURATION_SEC=$(($AFTER_DATE_SEC-$BEFORE_DATE_SEC));
 echo "> $DURATION_SEC secs FROM $BEFORE_DATE TO $AFTER_DATE";
-echo "> TEST ALL... DONE";
+echo "> LINT... DONE";
 echo "================================================================================";
