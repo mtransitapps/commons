bda2bd2e94a6ca48d9d3536fbf21167465100f04
Mathieu Méa <mathieu.mea@gmail.com>
2026-01-28 09:42:00 -0500

    Compat w/ GRT provider

--- shared-modules/app-android/src/main/MTAndroidManifest.xml.MT.sh
+++ shared-modules/app-android/src/main/MTAndroidManifest.xml.MT.sh
@@ -231,6 +231,26 @@ if [ -f "${CA_EDMONTON_PROVIDER_FILE}" ]; then
 EOL
 fi
 
+CA_GRAND_RIVER_TRANSIT_PROVIDER_FILE="${RES_VALUES_DIR}/grand_river_transit_values.xml";
+if [ -f "${CA_GRAND_RIVER_TRANSIT_PROVIDER_FILE}" ]; then
+  cat >>"${ANDROID_MANIFEST_FILE}" <<EOL
+        <!-- GRAND RIVER TRANSIT PROVIDER -->
+        <provider
+            android:name="org.mtransit.android.commons.provider.GrandRiverTransitProvider"
+            android:authorities="@string/grand_river_transit_authority"
+            android:exported="true"
+            android:readPermission="\${permission_provider_read}"
+            tools:ignore="MissingRegistered">
+            <meta-data
+                android:name="@string/status_provider"
+                android:value="@string/status_provider" />
+            <meta-data
+                android:name="@string/status_provider_target"
+                android:value="@string/grand_river_transit_status_for_poi_authority" />
+        </provider>
+EOL
+fi
+
 RSS_FILE="${RES_VALUES_DIR}/rss_values.xml";
 if [ -f "${RSS_FILE}" ]; then
   cat >>"${ANDROID_MANIFEST_FILE}" <<EOL
