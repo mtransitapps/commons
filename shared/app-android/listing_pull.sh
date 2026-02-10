32ce724f8cd2a803f1977210896ba0e7a080624f
Mathieu Méa <mathieu.mea@gmail.com>
2026-02-10 13:55:27 -0500

    Play store listing pull > ignore release notes (& subs & in-app products)

--- shared/app-android/listing_pull.sh
+++ shared/app-android/listing_pull.sh
@@ -14,7 +14,8 @@ checkResult $?;
 
 setGradleArgs;
 
-${SCRIPT_DIR}/../gradlew :app-android:bootstrapListing --app-details --listings --release-notes; # no ${GRADLE_ARGS} for release
+# Release notes are always pushed from repo (Alpha/Beta tracks are messing with Production)
+${SCRIPT_DIR}/../gradlew :app-android:bootstrapListing --app-details --listings --no-release-notes --no-products --no-subscriptions; # no ${GRADLE_ARGS} for release
 COMMAND_RESULT=$?; # save command result but cleanup keys 1st
 
 ${SCRIPT_DIR}/keys_cleanup.sh;
@@ -23,57 +24,59 @@ checkResult $?;
 checkResult $COMMAND_RESULT; # check command result after keys cleanup
 
 RELEASE_NOTES_DIR="${SCRIPT_DIR}/src/main/play/release-notes";
+git checkout -- ${RELEASE_NOTES_DIR};
+checkResult $?;
 
-OLD_IFS="$IFS"; # work w/ file name with spaces
-IFS=$'\n'; # work w/ file name with spaces
-for DIR in `find ${RELEASE_NOTES_DIR} -maxdepth 1 -type d ! -path ${RELEASE_NOTES_DIR}` ; do
-  echo ">> Directory: '$DIR'.";
-	for FILE_NAME in `find ${DIR} -maxdepth 1 -type f -name "*.txt"` ; do
-    # DEFAULT
-		if [[ "${FILE_NAME}" == "${DIR}/default.txt" ]]; then
-		  echo ">> - Default release note found.";
-    # PRODUCTION
-		elif [[ "${FILE_NAME}" == "${DIR}/production.txt" ]]; then
-      if [[ ! -f "${DIR}/default.txt" ]]; then
-        cp "${DIR}/production.txt" "${DIR}/default.txt";
-        checkResult $?;
-        echo ">> - Production release note used for default.";
-      fi
-      rm "${FILE_NAME}";
-      checkResult $?;
-      echo ">> - File '${FILE_NAME}' deleted.";
-    # BETA (Private)
-		elif [[ "${FILE_NAME}" == "${DIR}/Beta\ \(Private\).txt" ]]; then
-      if [[ ! -f "${DIR}/default.txt" ]]; then
-        cp "${DIR}/Beta\ \(Private\).txt" "${DIR}/default.txt";
-        checkResult $?;
-        echo ">> - Beta (Private) release note used for default.";
-      fi
-      rm "${FILE_NAME}";
-      checkResult $?;
-      echo ">> - File '${FILE_NAME}' deleted.";
-    # ALPHA
-    elif [[ "${FILE_NAME}" == "${DIR}/alpha.txt" ]]; then
-      if [[ ! -f "${DIR}/default.txt" ]]; then
-        cp "${DIR}/alpha.txt" "${DIR}/default.txt";
-        checkResult $?;
-        echo ">> - Alpha release note used for default.";
-      fi
-      rm "${FILE_NAME}";
-      checkResult $?;
-      echo ">> - File '${FILE_NAME}' deleted.";
-    # OTHER
-    else
-      rm "${FILE_NAME}";
-      checkResult $?;
-      echo ">> - File '${FILE_NAME}' deleted.";
-		fi
-	done
-	if [[ ! -f "${DIR}/default.txt" ]]; then
-    echo ">> - No default release note in '$DIR'!";
-    exit 1;
-  fi
-done
-IFS="$OLD_IFS"; # work w/ file name with spaces
+# OLD_IFS="$IFS"; # work w/ file name with spaces
+# IFS=$'\n'; # work w/ file name with spaces
+# for DIR in `find ${RELEASE_NOTES_DIR} -maxdepth 1 -type d ! -path ${RELEASE_NOTES_DIR}` ; do
+#   echo ">> Directory: '$DIR'.";
+# 	for FILE_NAME in `find ${DIR} -maxdepth 1 -type f -name "*.txt"` ; do
+#     # DEFAULT
+# 		if [[ "${FILE_NAME}" == "${DIR}/default.txt" ]]; then
+# 		  echo ">> - Default release note found.";
+#     # PRODUCTION
+# 		elif [[ "${FILE_NAME}" == "${DIR}/production.txt" ]]; then
+#       if [[ ! -f "${DIR}/default.txt" ]]; then
+#         cp "${DIR}/production.txt" "${DIR}/default.txt";
+#         checkResult $?;
+#         echo ">> - Production release note used for default.";
+#       fi
+#       rm "${FILE_NAME}";
+#       checkResult $?;
+#       echo ">> - File '${FILE_NAME}' deleted.";
+#     # BETA (Private)
+# 		elif [[ "${FILE_NAME}" == "${DIR}/Beta\ \(Private\).txt" ]]; then
+#       if [[ ! -f "${DIR}/default.txt" ]]; then
+#         cp "${DIR}/Beta\ \(Private\).txt" "${DIR}/default.txt";
+#         checkResult $?;
+#         echo ">> - Beta (Private) release note used for default.";
+#       fi
+#       rm "${FILE_NAME}";
+#       checkResult $?;
+#       echo ">> - File '${FILE_NAME}' deleted.";
+#     # ALPHA
+#     elif [[ "${FILE_NAME}" == "${DIR}/alpha.txt" ]]; then
+#       if [[ ! -f "${DIR}/default.txt" ]]; then
+#         cp "${DIR}/alpha.txt" "${DIR}/default.txt";
+#         checkResult $?;
+#         echo ">> - Alpha release note used for default.";
+#       fi
+#       rm "${FILE_NAME}";
+#       checkResult $?;
+#       echo ">> - File '${FILE_NAME}' deleted.";
+#     # OTHER
+#     else
+#       rm "${FILE_NAME}";
+#       checkResult $?;
+#       echo ">> - File '${FILE_NAME}' deleted.";
+# 		fi
+# 	done
+# 	if [[ ! -f "${DIR}/default.txt" ]]; then
+#     echo ">> - No default release note in '$DIR'!";
+#     exit 1;
+#   fi
+# done
+# IFS="$OLD_IFS"; # work w/ file name with spaces
 
 echo ">> Pulling listing from Google Play Console... DONE";
