9ece5a4751b051397221665cf40caab59f21659d
Mathieu Méa <mathieu.mea@gmail.com>
2026-04-22 10:39:26 -0400

    `mt-sync-code-data.yml` > image gen > nice to have (#699)

--- shared-modules/app-android/src/main/play/listings/en-US/graphics/feature-graphic/MT1.png.MT.sh
+++ shared-modules/app-android/src/main/play/listings/en-US/graphics/feature-graphic/MT1.png.MT.sh
@@ -28,8 +28,6 @@ if [ -f "${FILE_1_PNG}" ]; then
     exit 0; # compat w/ manually created file
   else
     echo ">> File '$FILE_1_PNG' already exist: overriding image... (MT_GENERATE_IMAGES=$MT_GENERATE_IMAGES)";
-    rm -f "${FILE_1_PNG}";
-    checkResult $?;
   fi
 fi
 
