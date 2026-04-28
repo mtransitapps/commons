a16c979aada81672c296b7d582322aa0c5454aa6
Mathieu Méa <mathieu.mea@gmail.com>
2026-04-28 08:33:27 -0400

    Add promote from Prod to Prod 50%

---
+++ shared/app-android/promote_from_production_to_production_050.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$0")";
+source "${SCRIPT_DIR}"/../commons/commons.sh;
+
+setPushToStoreEnabled;
+if [[ ${MT_PUSH_STORE_ENABLED} != true ]]; then
+  echo "> Push to Store NOT enabled... SKIP ($MT_PUSH_STORE_ENABLED)";
+  exit 1; # error
+fi
+echo "> Push to Store enabled...";
+
+setPushToStoreProductionEnabled;
+if [[ ${MT_PUSH_STORE_PRODUCTION_ENABLED} != true ]]; then
+  echo "> Push to Store Production NOT enabled... SKIP ($MT_PUSH_STORE_PRODUCTION_ENABLED)";
+  exit 1; # error
+fi
+echo "> Push to Store Production enabled...";
+
+setGitProjectName $SCRIPT_DIR/../;
+CONFIG_PATH="$SCRIPT_DIR/../config";
+if [[ $GIT_PROJECT_NAME == *"-gradle"* ]]; then # OLD REPO
+  CONFIG_PATH="$SCRIPT_DIR/config";
+fi
+
+if [[ ! -f "$CONFIG_PATH/store/production" ]]; then
+    echo "> Publish production NOT authorized!";
+    exit 1; # error
+fi
+
+./promote.sh \
+  --from-track production --promote-track production \
+  --update production --release-status inProgress --user-fraction 0.50 \
+;
