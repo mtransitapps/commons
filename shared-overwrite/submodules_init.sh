8b3ad9ca7681a218312acded79e24ed31458adc9
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-10 09:36:35 -0400

    Required changes for templates

--- shared-overwrite/submodules_init.sh
+++ shared-overwrite/submodules_init.sh
@@ -16,6 +16,12 @@ function setGitProjectName() { # copy from commons.sh
 	fi
 }
 
+echo "================================================================================";
+echo "> INIT SUBMODULES...";
+echo "--------------------------------------------------------------------------------";
+BEFORE_DATE=$(date +%D-%X);
+BEFORE_DATE_SEC=$(date +%s);
+
 echo "> GitHub Actions: $GITHUB_ACTIONS.";
 
 IS_SHALLOW=$(git rev-parse --is-shallow-repository);
@@ -34,33 +40,81 @@ fi
 
 setGitProjectName;
 
+CURRENT_PATH=$(pwd);
+CURRENT_DIRECTORY=$(basename ${CURRENT_PATH});
+
+INIT_SUBMODULE=false;
+if [[ -f "$CURRENT_PATH/.gitmodules" ]]; then
+	INIT_SUBMODULE=false;
+else
+	INIT_SUBMODULE=true;
+fi
+
+# SHARED SUBMODULES
 declare -a SUBMODULES=(
 	"commons"
 	"commons-java"
 	"commons-android"
 );
+
+# APP-ANDROID (OLD REPO)
 if [[ "$GIT_PROJECT_NAME" == *"-gradle"* ]]; then # OLD REPO
 	SUBMODULES+=('app-android'); # OLD REPO
 fi
-if [[ -d "parser" ]]; then
-    SUBMODULES+=('parser');
-	if [[ "$GIT_PROJECT_NAME" == *"-gradle"* ]]; then # OLD REPO
-		SUBMODULES+=('agency-parser'); # OLD REPO
+
+#PARSER
+if [[ $PROJECT_NAME == "mtransit-for-android" ]]; then
+	echo "> Main android app: '$PROJECT_NAME' > parser required";
+	SUBMODULES+=('parser');
+	SUBMODULES_REPO+=('parser');
+elif [[ $PROJECT_NAME == *"-bike"* ]]; then
+	echo "> Bike android app: '$PROJECT_NAME' > parser NOT required";
+else
+	echo "> Bus/Train/... android app: '$PROJECT_NAME' > parser required";
+	SUBMODULES+=('parser');
+	SUBMODULES_REPO+=('parser');
+	mkdir -p agency-parser/archive; # needed for shared-opt-dir #InitRepo
+	if [[ "$INIT_SUBMODULE" == true ]]; then
+		git lfs track agency-parser/archive/*;
 	fi
 fi
-echo "Submodules:";
-printf '* "%s"\n' "${SUBMODULES[@]}";
+
+echo "> Submodules:";
+printf '> - "%s"\n' "${SUBMODULES[@]}";
 
 for SUBMODULE in "${SUBMODULES[@]}" ; do
-	if ! [[ -d "$SUBMODULE" ]]; then
-		echo "> Submodule does NOT exist '$SUBMODULE'!";
+    echo "--------------------------------------------------------------------------------";
+    if [[ "$INIT_SUBMODULE" == true ]]; then # ADDNING GIT SUBMODULE
+		if [[ -d "$CURRENT_PATH/$SUBMODULE" ]]; then
+			echo "> Cannot override '$CURRENT_PATH/$SUBMODULE'!";
+			exit 1;
+		fi
+		echo "> Adding submodule '$SUBMODULE'...";
+		git submodule add https://github.com/mtransitapps/$SUBMODULE.git $SUBMODULE; # GitHub secret PAT
+		RESULT=$?;
+		if [[ ${RESULT} -ne 0 ]]; then
+			echo "> Error while cloning '$SUBMODULE' submodule!";
+			exit ${RESULT};
+		fi
+		echo "> Adding submodule '$SUBMODULE'... DONE ✓";
+	fi
+	if ! [[ -d "$CURRENT_PATH/$SUBMODULE" ]]; then
+		echo "> Submodule directory '$CURRENT_PATH/$SUBMODULE' does NOT exist!";
 		exit 1;
 	fi
 	git submodule update --init --recursive "${SUBMODULE}";
 	RESULT=$?;
 	if [[ ${RESULT} -ne 0 ]]; then
-		echo "Error while update GIT submodule '$SUBMODULE'!";
+		echo "> Error while update GIT submodule '$SUBMODULE'!";
 		exit ${RESULT};
 	fi
 	echo "'$SUBMODULE' updated successfully."
 done
+
+echo "--------------------------------------------------------------------------------";
+AFTER_DATE=$(date +%D-%X);
+AFTER_DATE_SEC=$(date +%s);
+DURATION_SEC=$(($AFTER_DATE_SEC-$BEFORE_DATE_SEC));
+echo "> $DURATION_SEC secs FROM $BEFORE_DATE TO $AFTER_DATE";
+echo "> INIT SUBMODULES... DONE ✓";
+echo "================================================================================";
