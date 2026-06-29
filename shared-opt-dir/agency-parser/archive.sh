5dd677dc9f4a1b87dc423e2aab9de231bd33f6f5
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-29 19:22:35 -0400

    PR comments

--- shared-opt-dir/agency-parser/archive.sh
+++ shared-opt-dir/agency-parser/archive.sh
@@ -48,9 +48,9 @@ if [[ $FILE_CALENDAR_LINE -gt 1 ]]; then
   checkResult $?;
   CUT_START_DATE_INDEX=$((START_DATE_INDEX+1))
   CUT_END_DATE_INDEX=$((END_DATE_INDEX+1))
-  mapfile -t START_DATES < <(tail -n +2 "${FILE_CALENDAR}" | tr -d '\r' | cut -d ',' -f $CUT_START_DATE_INDEX | tr -d ' ')
+  mapfile -t START_DATES < <(tail -n +2 "${FILE_CALENDAR}" | cut -d ',' -f $CUT_START_DATE_INDEX | tr -d '\r ')
   cleanArray START_DATES
-  mapfile -t END_DATES < <(tail -n +2 "${FILE_CALENDAR}" | tr -d '\r' | cut -d ',' -f $CUT_END_DATE_INDEX | tr -d ' ')
+  mapfile -t END_DATES < <(tail -n +2 "${FILE_CALENDAR}" | cut -d ',' -f $CUT_END_DATE_INDEX | tr -d '\r ')
   cleanArray END_DATES
   readarray -t START_DATES_SORTED < <(printf '%s\n' "${START_DATES[@]}" | sort)
   readarray -t END_DATES_SORTED < <(printf '%s\n' "${END_DATES[@]}" | sort)
@@ -66,7 +66,7 @@ elif [[ -f "$FILE_CALENDAR_DATES" ]]; then
   DATE_INDEX=$(getArrayIndex HEADERS_ARRAY "date")
   checkResult $?;
   CUT_INDEX=$((DATE_INDEX+1))
-  mapfile -t DATES < <(tail -n +2 "${FILE_CALENDAR_DATES}" | tr -d '\r' | cut -d ',' -f $CUT_INDEX | tr -d ' ')
+  mapfile -t DATES < <(tail -n +2 "${FILE_CALENDAR_DATES}" | cut -d ',' -f $CUT_INDEX | tr -d '\r ')
   cleanArray DATES
   readarray -t DATES_SORTED < <(printf '%s\n' "${DATES[@]}" | sort)
   START_DATE=${DATES_SORTED[0]}
