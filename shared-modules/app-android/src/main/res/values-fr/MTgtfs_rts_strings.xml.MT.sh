3cc3395645d229a5a4e794965c0c8692d0eeb341
Mathieu Méa <mathieu.mea@gmail.com>
2026-08-21 08:37:20 -0400

    Generated XML files > escape `'`->`\'` (#849)

--- shared-modules/app-android/src/main/res/values-fr/MTgtfs_rts_strings.xml.MT.sh
+++ shared-modules/app-android/src/main/res/values-fr/MTgtfs_rts_strings.xml.MT.sh
@@ -117,6 +117,11 @@ fi
 # escape "&" -> "&amp;" for XML
 AGENCY_NAME_LONG="${AGENCY_NAME_LONG//&/&amp;}";
 AGENCY_NAME_SHORT="${AGENCY_NAME_SHORT//&/&amp;}";
+TYPE_LABEL="${TYPE_LABEL//&/&amp;}";
+# escape "'" -> "\'" for XML
+AGENCY_NAME_LONG="${AGENCY_NAME_LONG//\'/\\\'}";
+AGENCY_NAME_SHORT="${AGENCY_NAME_SHORT//\'/\\\'}";
+TYPE_LABEL="${TYPE_LABEL//\'/\\\'}";
 
 cat >>"${GTFS_RTS_STRINGS_FILE}" <<EOL
 <?xml version="1.0" encoding="utf-8"?>
