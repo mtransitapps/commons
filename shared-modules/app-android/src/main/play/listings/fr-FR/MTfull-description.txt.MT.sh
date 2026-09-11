a8815c7606576e04ce5a27a1b9d61d3e86d82e6a
Mathieu Méa <mathieu.mea@gmail.com>
2026-09-11 09:13:07 -0400

    Generated files > also use `play/listings/fr-FR` to  enable FR lang (#856)

--- shared-modules/app-android/src/main/play/listings/fr-FR/MTfull-description.txt.MT.sh
+++ shared-modules/app-android/src/main/play/listings/fr-FR/MTfull-description.txt.MT.sh
@@ -13,20 +13,23 @@ setIsCI;
 APP_ANDROID_DIR="${ROOT_DIR}/app-android";
 SRC_DIR="${APP_ANDROID_DIR}/src";
 MAIN_DIR="${SRC_DIR}/main";
-PLAY_DIR="${MAIN_DIR}/play";
-LISTINGS_DIR="${PLAY_DIR}/listings";
-FR_FR_DIR="${LISTINGS_DIR}/fr-FR";
+RES_DIR="${MAIN_DIR}/res";
+
+VALUES_FR_DIR="${RES_DIR}/values-fr";
+
+PLAY_LISTINGS_FR_FR_DIR="${MAIN_DIR}/play/listings/fr-FR/";
 
 LANG_FR_FILE="${ROOT_DIR}/config/lang/fr";
-if [[ ! -f "$LANG_FR_FILE" && ! -d "$FR_FR_DIR" ]]; then
+
+if [[ ! -f "$LANG_FR_FILE" && ! -d "$VALUES_FR_DIR" && ! -d "$PLAY_LISTINGS_FR_FR_DIR" ]]; then
     echo ">> Generating fr-FR/full-description.txt... SKIP (FR lang not supported)";
     exit 0; # ok
 fi
 
 echo ">> Generating fr-FR/full-description.txt...";
 
-FULL_DESCRIPTION_FILE="${FR_FR_DIR}/full-description.txt";
-mkdir -p "${FR_FR_DIR}";
+FULL_DESCRIPTION_FILE="${PLAY_LISTINGS_FR_FR_DIR}/full-description.txt";
+mkdir -p "${PLAY_LISTINGS_FR_FR_DIR}";
 checkResult $?;
 if [ -f "${FULL_DESCRIPTION_FILE}" ]; then
   echo ">> File '$FULL_DESCRIPTION_FILE' already exist."; # compat with existing fr-FR/full-description.txt
--- shared-modules/app-android/src/main/res/values-fr/MTstrings.xml.MT.sh
+++ shared-modules/app-android/src/main/res/values-fr/MTstrings.xml.MT.sh
@@ -13,10 +13,14 @@ APP_ANDROID_DIR="${ROOT_DIR}/app-android";
 SRC_DIR="${APP_ANDROID_DIR}/src";
 MAIN_DIR="${SRC_DIR}/main";
 RES_DIR="${MAIN_DIR}/res";
+
 VALUES_FR_DIR="${RES_DIR}/values-fr";
 
+PLAY_LISTINGS_FR_FR_DIR="${MAIN_DIR}/play/listings/fr-FR/";
+
 LANG_FR_FILE="${ROOT_DIR}/config/lang/fr";
-if [[ ! -f "$LANG_FR_FILE" && ! -d "$VALUES_FR_DIR" ]]; then
+
+if [[ ! -f "$LANG_FR_FILE" && ! -d "$VALUES_FR_DIR" && ! -d "$PLAY_LISTINGS_FR_FR_DIR" ]]; then
     echo ">> Generating values-fr/strings.xml... SKIP (FR lang not supported)";
     exit 0; # ok
 fi
