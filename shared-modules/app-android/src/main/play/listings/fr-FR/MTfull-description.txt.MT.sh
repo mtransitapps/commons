68222fd8b63f315d12364ac6a2747f47bf65216d
Mathieu Méa <mathieu.mea@gmail.com>
2026-09-11 08:40:09 -0400

    Generated files > also use `play/listings/fr-FR` to  enable FR lang

--- shared-modules/app-android/src/main/play/listings/fr-FR/MTfull-description.txt.MT.sh
+++ shared-modules/app-android/src/main/play/listings/fr-FR/MTfull-description.txt.MT.sh
@@ -13,12 +13,15 @@ setIsCI;
 APP_ANDROID_DIR="${ROOT_DIR}/app-android";
 SRC_DIR="${APP_ANDROID_DIR}/src";
 MAIN_DIR="${SRC_DIR}/main";
-PLAY_DIR="${MAIN_DIR}/play";
-LISTINGS_DIR="${PLAY_DIR}/listings";
-FR_FR_DIR="${LISTINGS_DIR}/fr-FR";
+RES_DIR="${MAIN_DIR}/res";
+
+VALUES_FR_DIR="${RES_DIR}/values-fr";
+
+PLAY_LISTINGS_FR_FR_DIR="${MAIN_DIR}/play/listings/fr-FR/";
 
 LANG_FR_FILE="${ROOT_DIR}/config/lang/fr";
-if [[ ! -f "$LANG_FR_FILE" && ! -d "$FR_FR_DIR" ]]; then
+
+if [[ ! -f "$LANG_FR_FILE" && ! -d "$VALUES_FR_DIR" && ! -d "$PLAY_LISTINGS_FR_FR_DIR" ]]; then
     echo ">> Generating fr-FR/full-description.txt... SKIP (FR lang not supported)";
     exit 0; # ok
 fi
